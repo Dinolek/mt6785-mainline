ddcc67502e5158156b5c41814903fd1ea2332452
Huang Yiwei <hyiwei@codeaurora.org>
2021-09-15 10:46:21 +0800

    Revert "genirq: Restrict export of irq_to_desc()"
    This reverts commit 64a1b95bb9fe3ec76e1a2cd803eff06389341ae4 and
    commit 11cc92eb747aace5aa2b54b65b5cb8325a8981de.
    
    Commit 64a1b95bb9fe ("genirq: Restrict export of irq_to_desc()") removed
    the export of irq_to_desc() and commit 11cc92eb747a ("genirq: Fix export
    of irq_to_desc() for powerpc KVM") added it back with the powerpc KVM
    limitation. But there's still some usage in GKI kernel modules, so
    revert both two commits.
    
    Bug: 199823932
    Signed-off-by: Huang Yiwei <hyiwei@codeaurora.org>
    Change-Id: I3f871ecb5bc002c8643b4ddd3137a6899ac77b4f

--- kernel/irq/irqdesc.c
+++ kernel/irq/irqdesc.c
@@ -415,9 +415,7 @@ struct irq_desc *irq_to_desc(unsigned int irq)
 {
 	return mtree_load(&sparse_irqs, irq);
 }
-#ifdef CONFIG_KVM_BOOK3S_64_HV_MODULE
-EXPORT_SYMBOL_GPL(irq_to_desc);
-#endif
+EXPORT_SYMBOL(irq_to_desc);
 
 void irq_lock_sparse(void)
 {
